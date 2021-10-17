fc61ff9fdf90c0e629399bf7c361c8bc5fc03b75
kaischaefer2000 <kai305@web.de>
2021-10-17 18:01:29 +0200

    some more seo

--- phpFiles/index.php
+++ phpFiles/index.php
@@ -60,7 +60,7 @@
         <section>
             <div class="row">
                 <div class="col heading">
-                    <h1>News from<span translate="no">Lesson in Violence</span></h1>
+                    <h2>News from<span translate="no">Lesson in Violence</span></h2>
                 </div>
             </div>
     
