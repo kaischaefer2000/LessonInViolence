2d600479f6ef9748a0a27802bf4e1d4116ae36eb
kaischaefer2000 <kai305@web.de>
2023-09-11 11:22:41 +0200

    refactor live.js

--- jsFiles/live.js
+++ jsFiles/live.js
@@ -1,4 +1,4 @@
-// TODO: refactor !!!!!
+/// Accordeon functionality to expand the live shows.
 var acc = document.getElementsByClassName("show");
 var i;
 
@@ -15,236 +15,48 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// click event for LessonsInMetalFlyer
-img2 = document.getElementById("LessonsInMetalFlyer");
+/// Maximize / minimize flyers.
+function toggleBigImg(imgId, maxWidth) {
+  const img = document.getElementById(imgId);
+  img.classList.toggle("bigImg");
+  img.style.width = img.classList.contains("bigImg") ? maxWidth : "15vw";
+  img.style.transition = "width 0.8s ease-in-out";
+}
 
-function scaleImg2() {
-  if (img2.classList.contains("bigImg")) {
-    img2.classList.remove("bigImg");
-    img2.style.width = "15vw";
-    img2.style.transition = "width 0.8s ease-in-out";
+function addClickEvent(imgInfo) {
+  const img = document.getElementById(imgInfo.id);
+  img.addEventListener("click", () =>
+    toggleBigImg(imgInfo.id, imgInfo.maxWidth)
+  );
+}
+
+const imgData = [
+  { id: "LessonsInMetalFlyer", maxWidth: "30vw" },
+  { id: "birthdayBashFlyer", maxWidth: "35vw" },
+  { id: "JugendhausEinsFlyer", maxWidth: "40vw" },
+  { id: "InfernumFlyer", maxWidth: "25vw" },
+  { id: "MoshpitFlyer", maxWidth: "30vw" },
+  { id: "ZyklopFlyer", maxWidth: "30vw" },
+  { id: "MetalMassacreFlyer", maxWidth: "30vw" },
+  { id: "LIM3Flyer", maxWidth: "30vw" },
+  { id: "ParasitePitFlyer", maxWidth: "30vw" },
+  { id: "BM23", maxWidth: "30vw" },
+  { id: "MetalDayzFlyer", maxWidth: "45vw" },
+  { id: "BastardFlyer", maxWidth: "45vw" },
+];
+
+imgData.forEach(addClickEvent);
+
+const x = window.matchMedia("(min-width: 750px)");
+x.addEventListener("change", (e) => {
+  if (e.matches) {
+    imgData.forEach(addClickEvent);
   } else {
-    img2.style.width = "30vw";
-    img2.style.transition = "width 0.8s ease-in-out";
-    img2.classList.add("bigImg");
+    imgData.forEach((imgInfo) => {
+      const img = document.getElementById(imgInfo.id);
+      img.removeEventListener("click", () =>
+        toggleBigImg(imgInfo.id, imgInfo.maxWidth)
+      );
+    });
   }
-}
-
-// click event for InfernumFlyer
-img5 = document.getElementById("InfernumFlyer");
-
-function scaleImg5() {
-  if (img5.classList.contains("bigImg")) {
-    img5.classList.remove("bigImg");
-    img5.style.width = "15vw";
-    img5.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img5.style.width = "30vw";
-    img5.style.transition = "width 0.8s ease-in-out";
-    img5.classList.add("bigImg");
-  }
-}
-
-// click event for Birthday Bash Flyer
-img3 = document.getElementById("birthdayBashFlyer");
-
-function scaleImg3() {
-  if (img3.classList.contains("bigImg")) {
-    img3.classList.remove("bigImg");
-    img3.style.width = "15vw";
-    img3.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img3.style.width = "30vw";
-    img3.style.transition = "width 0.8s ease-in-out";
-    img3.classList.add("bigImg");
-  }
-}
-
-// click event for Jugendhaus eins Flyer
-img4 = document.getElementById("JugendhausEinsFlyer");
-
-function scaleImg4() {
-  if (img4.classList.contains("bigImg")) {
-    img4.classList.remove("bigImg");
-    img4.style.width = "15vw";
-    img4.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img4.style.width = "30vw";
-    img4.style.transition = "width 0.8s ease-in-out";
-    img4.classList.add("bigImg");
-  }
-}
-
-// click event for Birthday Moshpit
-img6 = document.getElementById("MoshpitFlyer");
-
-function scaleImg6() {
-  if (img6.classList.contains("bigImg")) {
-    img6.classList.remove("bigImg");
-    img6.style.width = "15vw";
-    img6.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img6.style.width = "30vw";
-    img6.style.transition = "width 0.8s ease-in-out";
-    img6.classList.add("bigImg");
-  }
-}
-
-// click event for Zyklop
-img7 = document.getElementById("ZyklopFlyer");
-
-function scaleImg7() {
-  if (img7.classList.contains("bigImg")) {
-    img7.classList.remove("bigImg");
-    img7.style.width = "15vw";
-    img7.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img7.style.width = "30vw";
-    img7.style.transition = "width 0.8s ease-in-out";
-    img7.classList.add("bigImg");
-  }
-}
-
-// click event for metal massacre
-img8 = document.getElementById("MetalMassacreFlyer");
-
-function scaleImg8() {
-  if (img8.classList.contains("bigImg")) {
-    img8.classList.remove("bigImg");
-    img8.style.width = "15vw";
-    img8.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img8.style.width = "30vw";
-    img8.style.transition = "width 0.8s ease-in-out";
-    img8.classList.add("bigImg");
-  }
-}
-
-// click event for LIM3
-img9 = document.getElementById("LIM3Flyer");
-
-function scaleImg9() {
-  if (img9.classList.contains("bigImg")) {
-    img9.classList.remove("bigImg");
-    img9.style.width = "15vw";
-    img9.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img9.style.width = "30vw";
-    img9.style.transition = "width 0.8s ease-in-out";
-    img9.classList.add("bigImg");
-  }
-}
-
-// click event for LIM3
-img10 = document.getElementById("LIM3Flyer");
-
-function scaleImg9() {
-  if (img9.classList.contains("bigImg")) {
-    img9.classList.remove("bigImg");
-    img9.style.width = "15vw";
-    img9.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img9.style.width = "30vw";
-    img9.style.transition = "width 0.8s ease-in-out";
-    img9.classList.add("bigImg");
-  }
-}
-// click event for LIM3
-img10 = document.getElementById("ParasitePitFlyer");
-
-function scaleImg10() {
-  if (img10.classList.contains("bigImg")) {
-    img10.classList.remove("bigImg");
-    img10.style.width = "15vw";
-    img10.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img10.style.width = "30vw";
-    img10.style.transition = "width 0.8s ease-in-out";
-    img10.classList.add("bigImg");
-  }
-}
-
-// click event for BM23
-img11 = document.getElementById("BM23");
-
-function scaleImg11() {
-  if (img11.classList.contains("bigImg")) {
-    img11.classList.remove("bigImg");
-    img11.style.width = "15vw";
-    img11.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img11.style.width = "30vw";
-    img11.style.transition = "width 0.8s ease-in-out";
-    img11.classList.add("bigImg");
-  }
-}
-
-// click event for MetalDayzFlyer
-img12 = document.getElementById("MetalDayzFlyer");
-
-function scaleImg12() {
-  if (img12.classList.contains("bigImg")) {
-    img12.classList.remove("bigImg");
-    img12.style.width = "15vw";
-    img12.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img12.style.width = "45vw";
-    img12.style.transition = "width 0.8s ease-in-out";
-    img12.classList.add("bigImg");
-  }
-}
-
-// click event for Bastards Bash
-img13 = document.getElementById("BastardFlyer");
-
-function scaleImg13() {
-  if (img13.classList.contains("bigImg")) {
-    img13.classList.remove("bigImg");
-    img13.style.width = "15vw";
-    img13.style.transition = "width 0.8s ease-in-out";
-  } else {
-    img13.style.width = "45vw";
-    img13.style.transition = "width 0.8s ease-in-out";
-    img13.classList.add("bigImg");
-  }
-}
-
-// click event only when screen is big enough
-function myFunction(x) {
-  if (x.matches) {
-    document
-      .getElementById("LessonsInMetalFlyer")
-      .addEventListener("click", scaleImg2);
-    document
-      .getElementById("birthdayBashFlyer")
-      .addEventListener("click", scaleImg3);
-    document
-      .getElementById("JugendhausEinsFlyer")
-      .addEventListener("click", scaleImg4);
-    document
-      .getElementById("InfernumFlyer")
-      .addEventListener("click", scaleImg5);
-    document
-      .getElementById("MoshpitFlyer")
-      .addEventListener("click", scaleImg6);
-    document.getElementById("ZyklopFlyer").addEventListener("click", scaleImg7);
-    document
-      .getElementById("MetalMassacreFlyer")
-      .addEventListener("click", scaleImg8);
-    document.getElementById("LIM3Flyer").addEventListener("click", scaleImg9);
-    document
-      .getElementById("ParasitePitFlyer")
-      .addEventListener("click", scaleImg10);
-    document.getElementById("BM23").addEventListener("click", scaleImg11);
-    document
-      .getElementById("MetalDayzFlyer")
-      .addEventListener("click", scaleImg12);
-    document
-      .getElementById("BastardFlyer")
-      .addEventListener("click", scaleImg13);
-  }
-}
-
-var x = window.matchMedia("(min-width: 750px)");
-myFunction(x);
-x.addListener(myFunction);
+});
