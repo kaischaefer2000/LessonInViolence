9be9f51036b27d2e7d0ca3dd64714bc477d58cc6
kaischaefer2000 <kai305@web.de>
2026-08-29 16:07:15 +0200

    update metal night flyer

--- jsFiles/live.js
+++ jsFiles/live.js
@@ -63,6 +63,7 @@ const imgData = [
   { id: "LIM6Flyer", maxWidth: "30vw" },
   { id: "MoshpitXFlyer", maxWidth: "30vw" },
   { id: "AssultFuerth", maxWidth: "30vw" },
+  { id: "MetalNightFlyer", maxWidth: "30vw" },
 ];
 
 imgData.forEach(addClickEvent);
